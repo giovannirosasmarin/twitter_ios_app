de1b0d7d2b5816bc08cd36ec74cfd8035883e2e9
Giovanni Rosas-Marin <marin@Giovannis-MacBook-Pro.local>
2019-02-24 20:13:31 -0800

    done

---
+++ Twitter/TweetTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  TweetTableViewCell.swift
+//  Twitter
+//
+//  Created by Giovanni Rosas-Marin on 2/23/19.
+//  Copyright © 2019 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetTableViewCell: UITableViewCell {
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var tweetContent: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+   
+    
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
